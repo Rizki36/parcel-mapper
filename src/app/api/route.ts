822035751efc3ab1487fff07fe0ccfcab578283d
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-12-04 20:00:45 +0700

    fix import prisma

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "@prismaorm/client";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export async function GET() {
   const users = await prisma.user.findMany();

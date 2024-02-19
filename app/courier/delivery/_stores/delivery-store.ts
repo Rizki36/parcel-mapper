87d21b294ab0598f8a7e00650e8dd18a8e1ea141
Fitra R <39044004+Rizki36@users.noreply.github.com>
2024-02-19 08:40:28 +0700

    feat: add turf

--- app/courier/delivery/_stores/delivery-store.ts
+++ app/courier/delivery/_stores/delivery-store.ts
@@ -1,25 +1,39 @@
 // src/stores/counter-store.ts
 import { createStore } from "zustand/vanilla";
 
+export type Node = {
+  id: number;
+  lat: number;
+  lng: number;
+  type: "branch" | "customer";
+};
+
 export type DeliveryState = {
   step: number;
   distances: number[][]; // n x n matrix
+  nodes: Node[];
 };
 
 export type DeliveryActions = {
   setStep: (_number: number) => void;
   setDistances: (_distances: number[][]) => void;
+  setNodes: (_nodes: Node[]) => void;
 };
 
 export type DeliveryStore = DeliveryState & DeliveryActions;
 
 export const initDeliveryStore = (): DeliveryState => {
-  return { step: 1, distances: [] };
+  return {
+    step: 1,
+    distances: [],
+    nodes: [],
+  };
 };
 
 export const defaultInitState: DeliveryState = {
   step: 0,
   distances: [],
+  nodes: [],
 };
 
 export const createDeliveryStore = (
@@ -29,5 +43,6 @@ export const createDeliveryStore = (
     ...initState,
     setStep: (step) => set({ step }),
     setDistances: (distances) => set({ distances }),
+    setNodes: (nodes) => set({ nodes }),
   }));
 };
--- app/courier/delivery/page.tsx
+++ app/courier/delivery/page.tsx
@@ -1,29 +1,136 @@
 "use client";
 import { ENV } from "@/_constants";
 import { Box } from "@chakra-ui/react";
-import React from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useEffect, useMemo, useRef } from "react";
+import ReactMapGL, { MapRef, Marker } from "react-map-gl";
 import Drawer from "./_components/Drawer";
-import { DeliveryStoreProvider } from "./_providers/DeliveryProviders";
+import {
+  DeliveryStoreProvider,
+  useDeliveryStore,
+} from "./_providers/DeliveryProviders";
+import { IoLocation } from "react-icons/io5";
 
 const DeliveryPage = () => {
   return (
     <DeliveryStoreProvider>
-      <Box position="relative">
-        <ReactMapGL
-          reuseMaps
-          mapboxAccessToken={ENV.MAPBOX_ACCESS_TOKEN}
-          initialViewState={{
-            longitude: 112.226479,
-            latitude: -7.546839,
-            zoom: 14,
-          }}
-          style={{ height: "100dvh", maxHeight: "100dvh" }}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-        ></ReactMapGL>
-        <Drawer />
-      </Box>
+      <Content />
     </DeliveryStoreProvider>
+  );
+};
+
+const Content = () => {
+  const mapRef = useRef<MapRef>(null);
+  const { nodes, setNodes } = useDeliveryStore((state) => state);
+
+  const availableNodes = useMemo(() => {
+    return nodes?.filter((item) => item.lat && item.lng) || [];
+  }, [nodes]);
+
+  useEffect(() => {
+    setNodes([
+      {
+        id: 1,
+        lat: -7.551051361760125,
+        lng: 112.22348813042969,
+        type: "branch",
+      },
+      {
+        id: 2,
+        lat: -7.552858058777354,
+        lng: 112.22731316511666,
+        type: "customer",
+      },
+      {
+        id: 3,
+        lat: -7.555538060549121,
+        lng: 112.22803409207916,
+        type: "customer",
+      },
+      {
+        id: 4,
+        lat: -7.557300309104902,
+        lng: 112.23171116901788,
+        type: "customer",
+      },
+      {
+        id: 5,
+        lat: -7.5484007257025825,
+        lng: 112.2284502042292,
+        type: "customer",
+      },
+      {
+        id: 6,
+        lat: -7.550437801811475,
+        lng: 112.23595850239968,
+        type: "customer",
+      },
+    ]);
+  }, []);
+
+  const nodesMarker = useMemo(() => {
+    return availableNodes?.map((item) => (
+      <Marker
+        key={item.id}
+        longitude={item.lng as number}
+        latitude={item.lat as number}
+        anchor="bottom"
+        onClick={(e) => {
+          e.originalEvent.stopPropagation();
+        }}
+      >
+        <Box color="red">
+          <IoLocation
+            style={{
+              width: "20px",
+              height: "20px",
+            }}
+          />
+        </Box>
+      </Marker>
+    ));
+    // map
+  }, [availableNodes]);
+
+  // useEffect(() => {
+  //   if (!mapRef.current || !availableNodes.length) return;
+
+  //   const feature = featureCollection(
+  //     availableNodes.map((item) =>
+  //       point([item.lng, item.lat], {
+  //         id: item.id,
+  //       })
+  //     )
+  //   );
+
+  //   const [minLng, minLat, maxLng, maxLat] = bbox(feature);
+
+  //   mapRef.current?.fitBounds(
+  //     [
+  //       [minLng, minLat],
+  //       [maxLng, maxLat],
+  //     ],
+  //     { duration: 1000, zoom: 14 }
+  //   );
+  // }, [availableNodes, mapRef]);
+
+  return (
+    <Box position="relative">
+      <ReactMapGL
+        ref={mapRef}
+        reuseMaps
+        mapboxAccessToken={ENV.MAPBOX_ACCESS_TOKEN}
+        initialViewState={{
+          longitude: 112.226479,
+          latitude: -7.546839,
+          zoom: 14,
+        }}
+        style={{ height: "100dvh", maxHeight: "100dvh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        {nodesMarker}
+      </ReactMapGL>
+      <Drawer />
+    </Box>
   );
 };
 
